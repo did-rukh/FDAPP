9c291c01664b005c696ae1d91f7627bfebe2afca
didrukshratheesh2003@gmail.com <didrukshratheesh@2003@gmail.com>
2026-03-16 09:23:09 +0530

    Update backend and frontend code with new env and axios changes

--- frontend/food-frontend/src/api/axios.js
+++ frontend/food-frontend/src/api/axios.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://fdapp-cy7o.onrender.com",
+  baseURL: import.meta.env.VITE_BACKEND_URL, 
   // baseURL: process.env.REACT_APP_BACKEND_URL || "http://localhost:5000/api",
   
   withCredentials: true //  refresh token 
